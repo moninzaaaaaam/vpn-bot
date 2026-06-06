fa83f2de2f234ec829d544782836a5bd0484627c
moninzaaaaaam <mbyn36651@gmail.com>
2026-06-06 11:38:44 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,32 +9,34 @@ TOKEN = "8932326265:AAGxB2Oz3-XWc_PDO-ic5o3vGdJcILpsnuM"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("credit", callback_data="credit"),
-            InlineKeyboardButton("usage", callback_data="usage")
+            InlineKeyboardButton("💰 اعتبار من", callback_data="credit"),
+            InlineKeyboardButton("⏳ میزان مصرف", callback_data="usage")
+
         ],
         [
-            InlineKeyboardButton("referral", callback_data="referral"),
-            InlineKeyboardButton("tutorial", callback_data="tutorial")
+            InlineKeyboardButton("👥 معرفی به دوستان", callback_data="referral"),
+            InlineKeyboardButton("💡 آموزش", callback_data="tutorial")
+
         ],
         [
-            InlineKeyboardButton("support", callback_data="support")
+            InlineKeyboardButton("🧑 پشتیبانی", callback_data="support")
         ]
     ])
-    await update.message.reply_text("welcome", reply_markup=keyboard)
+    await update.message.reply_text("سلام خوش آمدید 🌿", reply_markup=keyboard)
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
     if query.data == "credit":
-        await query.edit_message_text("credit: 0")
+        await query.edit_message_text("اعتبار شما: 0 تومان")
     elif query.data == "usage":
-        await query.edit_message_text("usage: 0 GB")
+        await query.edit_message_text("مصرف: 0 GB")
     elif query.data == "referral":
-        await query.edit_message_text("referral link: t.me/Glassnetbot")
+        await query.edit_message_text("لینک: t.me/Glassnetbot")
     elif query.data == "tutorial":
-        await query.edit_message_text("tutorial: download - install - connect")
+        await query.edit_message_text("آموزش: دانلود - نصب - اتصال")
     elif query.data == "support":
-        await query.edit_message_text("support: @YourSupport")
+        await query.edit_message_text("پشتیبانی: @mobinzam")
 
 if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
